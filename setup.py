58fb1b20309af6cd9143aca58481f3589647a0dc
slashformotion <slashformotion@protonmail.com>
2020-07-30 21:53:56 +0200

    BLACK setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
 setup(
-    name='hivetoolkit',
+    name="hivetoolkit",
     version=__version__,
-    url='https://github.com/slashformotion/hivetools',
-    license='MIT',
-    author='Gigi Sayfan',
-    author_email='slashformotion@protonmail.com',
-    description='A set of tools to work with the Hive (or Steem) blockchain',
-    packages=['hivetoolkit'],
+    url="https://github.com/slashformotion/hivetools",
+    license="MIT",
+    author="Gigi Sayfan",
+    author_email="slashformotion@protonmail.com",
+    description="A set of tools to work with the Hive (or Steem) blockchain",
+    packages=["hivetoolkit"],
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
@@ -24,5 +24,6 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Topic :: Office/Business :: Financial"        
-    ])
+        "Topic :: Office/Business :: Financial",
+    ],
+)
