863d98a11eebcf89da5beccd5cc93eb44ee73e90
slashformotion <slashformotion@protonmail.com>
2020-07-31 20:01:10 +0200

    black reformat

--- tests/crawlers/crawlers/test_crawler.py
+++ tests/crawlers/crawlers/test_crawler.py
@@ -27,26 +27,26 @@ class test_Crawler(unittest.TestCase):
         self.assertFalse(hasattr(crawler, "start"))
         self.assertFalse(hasattr(crawler, "stop"))
 
-        start, stop = self._get_timeframe() 
+        start, stop = self._get_timeframe()
         crawler.set_timeframe(start, stop)
 
         self.assertTrue(hasattr(crawler, "start"))
         self.assertTrue(hasattr(crawler, "stop"))
-    
+
     def test__stream(self):
-        #without timeframe
+        # without timeframe
         crawler = self._makeOne(name="testing_crawler", blockchain="hive")
         for index, return_value in enumerate(crawler._stream()):
             if index <= 2:
                 break
             self.assertIsInstance(return_value, dict)
-            self.assertGreater(len(return_value.keys()),0)
-    
+            self.assertGreater(len(return_value.keys()), 0)
+
         crawler = self._makeOne(name="testing_crawler", blockchain="hive")
-        start, stop = self._get_timeframe() 
+        start, stop = self._get_timeframe()
         crawler.set_timeframe(start, stop)
         for index, return_value in enumerate(crawler._stream()):
             if index <= 2:
                 break
             self.assertIsInstance(return_value, dict)
-            self.assertGreater(len(return_value.keys()),0)
+            self.assertGreater(len(return_value.keys()), 0)
