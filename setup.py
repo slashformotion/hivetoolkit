ae6d3a1f65188a382dc722fcc5e4bd18d01661b7
slashformotion <slashformotion@protonmail.com>
2020-08-01 21:40:42 +0200

    fixed author

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.1dev2"
 
 setup(
     name="hivetoolkit",
     version=__version__,
     url="https://github.com/slashformotion/hivetools",
     license="MIT",
-    author="Gigi Sayfan",
+    author="slashformotion",
     author_email="slashformotion@protonmail.com",
     description="A set of tools to work with the Hive (or Steem) blockchain",
-    packages=["hivetoolkit"],
+    packages=find_packages(exclude=("tests",)),
+    install_requires=['beem'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
