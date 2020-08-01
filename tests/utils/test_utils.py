c3d8c80a08ea5018cef7c711c6971d059f8c3ef5
slashformotion <slashformotion@protonmail.com>
2020-08-01 23:00:12 +0200

    added test for hivetools.utils.intersection(lst1, lst2)

---
+++ tests/utils/test_utils.py
@@ -0,0 +1,21 @@
+from hivetoolkit.utils.utils import intersection
+import unittest
+
+class test_intersection(unittest.TestCase):
+
+    def _exe(self,*args, **kwargs):
+        return self._get_obj()(*args, **kwargs)
+
+    def _get_obj(self):
+        from hivetoolkit.utils import intersection
+
+        return intersection
+
+    def test_intersection(self):
+        lst1 = ['devtalk']
+        lst2 = ['dev', 'flask', 'devtalk', 'blog', 'tech']
+
+        self.assertListEqual(
+            self._exe(lst1, lst2),
+            ['devtalk']
+        )
